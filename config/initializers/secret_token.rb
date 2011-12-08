9bd28ffa578745f3fcf9e00d639290e5da5be22b
Artur Małecki <artur.malecki@aenima.pl>
2011-12-08 23:36:25 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NerveSynapse::Application.config.secret_token = '6cc87abd3dbfe8a87044536fc1fff150c53a752df9454e552c27a0d91a5836c6739c511b05ab5c19295cf3b76dff3b6f4cbe15f96427efd7c988468eaf6714fc'
