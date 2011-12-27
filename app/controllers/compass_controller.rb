c143ff7434425cc49ad9906520109b0744ec2c10
Artur Małecki <artur.malecki@aenima.pl>
2011-12-27 22:54:57 +0100

    Added user session id

--- app/controllers/compass_controller.rb
+++ app/controllers/compass_controller.rb
@@ -1,9 +1,10 @@
 class CompassController < ApplicationController
   def index
+    session[:user_session_id] = Digest::MD5::hexdigest "#{Time.now.to_s}#{rand}"
   end
 
   def get_image
-    new_image = Compass.get_image params
+    new_image = Compass.get_image params, session
     render :text => {
       :src => new_image
     }.to_json
--- app/models/compass.rb
+++ app/models/compass.rb
@@ -1,10 +1,11 @@
 class Compass
-  def self.get_image(params)
+  def self.get_image(params, session)
     resonance_core = ResonanceCore
     resonance_core.get(
       :bind,
       :click_area => (params[:click_area] || nil),
-      :clicked_at => Time.now.to_i
+      :clicked_at => Time.now.to_i,
+      :user_session_id => session[:user_session_id]
     )
   end
 end
