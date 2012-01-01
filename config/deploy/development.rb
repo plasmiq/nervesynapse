001178d18aa1763da93cef6e7fc5af31a7dedbba
Artur Małecki <artur.malecki@aenima.pl>
2012-01-01 19:36:14 +0100

    Added assets generator

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -18,3 +18,13 @@ task :after_update_code do
   files = %w(database.yml application.yml)
   files.each {|file| run "ln -nfs #{shared_path}/etc/#{file} #{release_path}/config/#{file}" }
 end
+
+namespace :deploy do
+  desc "precompile the assets"
+  task :precompile_assets, :roles => :web, :except => { :no_release => true } do
+    run "cd #{current_path}; rm -rf public/assets/*"
+    run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+  end
+end
+
+before "deploy:restart", "deploy:precompile_assets"
