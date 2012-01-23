2ba6d8f8710d6f255960fcb4e9263989e34700a2
Artur Małecki <artur.malecki@aenima.pl>
2012-01-23 10:53:46 +0100

    Added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  USER, PASSWORD = 'nervesynapse', 'gen001'
+ 
+  before_filter :authentication_check
+ 
+  private
+    def authentication_check
+      authenticate_or_request_with_http_basic do |user, password|
+        user == USER && password == PASSWORD
+      end
+    end
 end
